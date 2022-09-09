1927c2402b9942d10a6a2ccbacda40a3499a6361
Samara Silvia <100232025+SamaraSilvia81@users.noreply.github.com>
2022-09-09 20:07:11 -0300

    Add files via upload

---
+++ ExerciciosDeHeranca/src/questao01/Assistente.java
@@ -0,0 +1,31 @@
+package questao01;
+
+public class Assistente extends Funcionario {
+	
+	private int numMatricula;
+
+	public Assistente(String nome, double salario, int numMatricula) {
+		super(nome, salario); // herdados da superClasse (funcionário)
+		this.numMatricula = numMatricula;
+		
+		// a ordem dos argumentos importa no super - invoca o construtor da superClasse -
+	}
+
+	public int getNumMatricula() {
+		return numMatricula;
+	}
+
+	public void setNumMatricula(int numMatricula) {
+		this.numMatricula = numMatricula;
+	}
+
+	@Override   // polimorfismo
+	public void exibeDados() {
+		super.exibeDados();
+		System.out.println("Número de Matrícula: " + this.numMatricula);
+	}
+	
+	
+	
+
+}
